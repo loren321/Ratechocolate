7d2a7e8cca8e4b32c7a9093f27728578f7c01530
Loren Malčec <loren.malcec@yahoo.com>
2018-03-05 20:25:57 +0100

    Added empty Rating model and ratings table migration

---
+++ database/migrations/2018_03_05_191041_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('chocolate_id');
+            $table->integer('stars');
+            $table->text('review_text');
+            $table->integer('author_id');
+            $table->dateTime('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
