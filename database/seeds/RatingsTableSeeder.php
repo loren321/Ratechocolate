f42fdae9c01ffc0adc77fdd476451da004197eb5
Loren Malčec <loren.malcec@yahoo.com>
2018-03-08 20:27:31 +0100

    Made the Ratings seeder

---
+++ database/seeds/RatingsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+use App\Rating;
+
+class RatingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1, 30) as $index)
+        {
+            $rating = new Rating;
+            $rating->chocolate_id = $faker->numberBetween($min = 0, $max = 30);
+            $rating->stars = $faker->numberBetween($min = 1, $max = 5);
+            $rating->review_text = $faker->text($maxNbChars = 200);
+            $rating->author_id = $faker->numberBetween($min = 0, $max = 10);
+
+            $rating->save();
+        }
+    }
+}
