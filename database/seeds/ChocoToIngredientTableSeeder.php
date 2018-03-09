f2de6cdb59ee81a6d1e3fcbc39a9c093e4fdc6d2
Loren Malčec <loren.malcec@yahoo.com>
2018-03-09 22:43:17 +0100

    made the seeder for choco_to_ingredients pivot table

---
+++ database/seeds/ChocoToIngredientTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class ChocoToIngredientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1, 75) as $index)
+        {
+            DB::table('choco_to_ingredients')->insert([
+                'chocolate_id' => $faker->numberBetween($min = 0, $max = 30),
+                'ingredient_id' => $faker->numberBetween($min = 0, $max = 30)
+            ]);
+        }
+    }
+}
