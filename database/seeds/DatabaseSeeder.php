2a538e0ab1ecea3ede575a008311cf32d158a668
Loren Malčec <loren.malcec@yahoo.com>
2018-06-24 15:07:52 +0200

    Added all the seeders to the Database seeder so I can seed the whole database with one command

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ChocolateTableSeeder::class);
+        $this->call(ChocoToIngredientTableSeeder::class);
+        $this->call(IngredientTableSeeder::class);
+        $this->call(ManufacturerTableSeeder::class);
+        $this->call(NutritionTableSeeder::class);
+        $this->call(RatingsTableSeeder::class);
+        $this->call(TypeTableSeeder::class);
     }
 }
