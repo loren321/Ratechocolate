75b5f15a739b1a5f394fb852fa7ddaa2a0407b7f
Loren Malčec <loren.malcec@yahoo.com>
2019-02-19 21:48:27 +0100

    added links to chocolate and manufacturer in user page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,8 +15,8 @@
         <div class="card p-3">
           <div class="card-block">
             <h3 class="card-title py-1">
-              {{ $rating->chocolate->name }}
-              <small>by {{ $rating->chocolate->manufacturer->name }}</small>
+            <a href="{{ route('chocolate.ratings',  ['chocolate_id' => $rating->chocolate->id]) }}">{{ $rating->chocolate->name }}</a>
+              <small>by <a href="{{ route('manufacturer', ['chocolate_id' => $rating->chocolate->manufacturer->id]) }}">{{ $rating->chocolate->manufacturer->name }}</a></small>
               <small class="text-muted">{{ $rating->chocolate->cocoa_percentage }}% cocoa</small>
             </h3>
             <span>[X stars]</span>
